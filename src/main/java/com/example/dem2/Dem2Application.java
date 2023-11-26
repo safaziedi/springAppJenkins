c5eb2e4cfeacd3672810c127756cbcefa96a6807
safaziedi <safaziedi070@gmail.com>
2023-11-26 19:23:47 +0100

    first commit

---
+++ src/main/java/com/example/dem2/Dem2Application.java
@@ -0,0 +1,13 @@
+package com.example.dem2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dem2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dem2Application.class, args);
+	}
+
+}
