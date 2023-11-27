6e182a96e9cac9908e79d13fc2f65d023e518a98
safaziedi <safaziedi070@gmail.com>
2023-11-27 09:08:33 +0100

    add hello world

--- src/main/java/com/example/dem2/Dem2Application.java
+++ src/main/java/com/example/dem2/Dem2Application.java
@@ -2,9 +2,17 @@ package com.example.dem2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Dem2Application {
+
+	@GetMapping
+	String getMessage(){
+		return "Hello World!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Dem2Application.class, args);
